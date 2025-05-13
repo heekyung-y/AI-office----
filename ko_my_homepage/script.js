e9e00e54ef1ac0d863adcf248816a78bb60f0a52
heekyung-y <youhk06@gmail.com>
2025-05-13 13:58:56 +0900

    Add files via upload

---
+++ ko_my_homepage/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const links = document.querySelectorAll("nav a");
+  links.forEach(link => {
+    link.addEventListener("click", function () {
+      links.forEach(l => l.classList.remove("active"));
+      this.classList.add("active");
+    });
+  });
+});
